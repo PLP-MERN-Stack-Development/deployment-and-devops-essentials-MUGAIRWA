e9a9819bd6bf6f03b973b34f29c6c44682f4e988
MUGAIRWA <agalatevinmugairwa@gmail.com>
2025-11-20 15:17:06 +0300

    UPDATED My assignment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@radix-ui/react-dialog'],
+          clerk: ['@clerk/clerk-react'],
+          socket: ['socket.io-client'],
+        },
+      },
+    },
+    sourcemap: false,
+    minify: 'terser',
+  },
 })
